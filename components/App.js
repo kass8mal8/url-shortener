785b19ebc3dc6532028b93b8f9e7e8a249ae307e
kass8mal8 <kassimaly21@gmail.com>
2022-11-01 18:00:32 +0300

    initial commit

---
+++ components/App.js
@@ -0,0 +1,17 @@
+import Article from "./Article";
+import Footer from "./Footer";
+import Header from "./Header";
+import Main from "./Main";
+
+const App =()=> {
+  return (
+    <div className="App">
+      <Header />
+      <Main />
+      <Article />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
