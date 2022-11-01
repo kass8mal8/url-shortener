0028990c05ffc4ddfa9be5f90e05d25732d69c8c
kass8mal8 <kassimaly21@gmail.com>
2022-11-01 18:47:14 +0300

    initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import Article from "./Article";
 import Footer from "./Footer";
 import Header from "./Header";
 import Main from "./Main";
+import '../styles/app.css'
 
 const App =()=> {
   return (
